180c7a6833a7cc877e82b2ae8daa698bbdaffff5
F̷ᴀᴀᴛʜɪN̷ <frweird@outlook.co.id>
2023-12-11 21:38:40 +0700

    Update deshc.sh

--- deshc.sh
+++ deshc.sh
@@ -2,7 +2,7 @@
 for arg in $@
 do
   $arg > /dev/null & child=$!
-  prlimit --pid $child --nofile=0:
+  #prlimit --pid $child --nofile=0:
   kill -STOP $child
   head -n 1 /proc/$child/cmdline | sed 's/.*\(#!\)/\1/; $d' > $arg.dec.sh
   kill -TERM $child
