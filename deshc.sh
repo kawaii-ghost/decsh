8201b4e0dcc243c284ea17528c0448df2f34113c
F̷ᴀᴀᴛʜɪN̷ <frweird@outlook.co.id>
2023-12-11 21:39:04 +0700

    Update deshc.sh

--- deshc.sh
+++ deshc.sh
@@ -2,7 +2,7 @@
 for arg in $@
 do
   $arg > /dev/null & child=$!
-  #prlimit --pid $child --nofile=0:
+  sleep 0.01
   kill -STOP $child
   head -n 1 /proc/$child/cmdline | sed 's/.*\(#!\)/\1/; $d' > $arg.dec.sh
   kill -TERM $child
