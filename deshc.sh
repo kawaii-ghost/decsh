1d4d69cd20d1f51cba3e5349982dfd1fbb4ca34b
I°_°I <6h8fn1zo@duck.com>
2024-06-08 16:58:49 +0700

    Adjust time

--- deshc.sh
+++ deshc.sh
@@ -2,7 +2,7 @@
 for arg in $@
 do
   $arg > /dev/null & child=$!
-  sleep 0.01
+  sleep 0.03
   kill -STOP $child
   cat /proc/$child/cmdline | sed 's/.*\(#!\)/\1/; $d' > $arg.dec.sh
   kill -TERM $child
